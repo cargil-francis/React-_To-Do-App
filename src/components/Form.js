950470133f8468e6224f24232ae022fc87ef03da
Cargil Francis <cargil@pop-os.localdomain>
2022-12-14 00:10:48 +0530

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Form = () =>{ 
+    return (
+        <form className="ui form">
+        <input type ="text"/>
+        </form>
+    );
+};
+
+
+export default Form;
