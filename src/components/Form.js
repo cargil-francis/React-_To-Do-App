b0de96b1a0cdf7b8bb21313ad16d6601519494d4
Cargil Francis <cargil@pop-os.localdomain>
2022-12-14 18:00:17 +0530

    first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,17 @@ import React from "react";
 const Form = () =>{ 
     return (
         <form className="ui form">
+            <div className="ui grid">
+                <div className="row">
+                    <div className="column five wide">
         <input type ="text"/>
+        </div>
+        <div className="column one wide">
+        <button type="Submit">+</button>
+        </div>
+       
+        </div>
+        </div>
         </form>
     );
 };
